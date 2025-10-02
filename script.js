25ce1bec9a07f7fefd14585712846d7e561aff01
CioazaIoan <cioazai@yahoo.com>
2025-10-02 18:54:06 +0300

    mesaj

---
+++ script.js
@@ -0,0 +1,42 @@
+// Adunare:
+let a = 10;
+let b = 5;
+console.log(a + b);
+
+// scadere
+
+let x = 20;
+let y = 8;
+console.log(x - y);
+
+// inmultire
+
+let m = 4;
+let n = 6;
+console.log(m * n);
+
+// impartire
+
+let num1 = 15;
+let num2 = 3;
+console.log(num1 / num2);
+
+// Modul (restul împărțirii):
+
+let num3 = 14;
+let num4 = 4;
+console.log(num3 % num4);
+
+// Exponențiere:
+
+let base = 3;
+let exponent = 4;
+console.log(base ** exponent);
+
+// Incrementare și decrementare:
+
+let val = 10;
+val++;
+console.log(val);
+val--;
+console.log(val);
